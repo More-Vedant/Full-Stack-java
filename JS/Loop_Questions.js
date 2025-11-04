945df5ecb9c211fccb051281407bd4058fc3b2bf
vedantmore27 <vedantmore894@gmail.com>
2025-11-04 13:42:08 +0530

    code of the day

---
+++ JS/Loop_Questions.js
@@ -0,0 +1,74 @@
+//1.Print numbers from 1 to 10 using a loop.//
+//For Loop//
+
+// let lastNum=10;
+// let i=1;
+
+// for (let i=1;i<=lastNum;i++){
+//     console.log("i");
+//}
+
+//While Loop//
+
+// i=1;
+// while(i<=lastNum);
+// console.log("i");
+// i++;
+
+//2.Print all even numbers between 1 and 50.//
+//For Loop//
+
+// let LastNum=50;
+// let i=1;
+
+// for (let i=1;i<=LastNum;i++){
+//     if(i%2==0){
+//         console.log("i");
+//     }
+//     else{
+//         console.log("i");
+//     }
+// }
+
+//while Loop//
+
+// let LastNum=50;
+// let i=1;
+
+// while(i<=LastNum){
+//     if(i%2==0){
+//         console.log("i");
+//     }
+//     else{
+//         console.log("i");
+//     }
+//     i++;
+// }
+
+//4.Print the multiplication table of 5.//
+
+
+
+//5.Print numbers from 100 down to 1 (reverse order).
+
+
+
+//Reverse Number 123.//
+
+// let num=123;
+// let value=num+'';
+// let jhola='';
+
+// for(let i=value.length-1;i>=0;i--){
+//     jhola+=value[i];
+// }
+// jhola=+jhola;
+// console.log(jhola);
+
+let num2=432;
+num2=num2+'';
+console.log(num2.length)
+let sum1=0;
+for(let i=0;i<num2.length;i++){
+    sum1+=+num2[i];
+}
