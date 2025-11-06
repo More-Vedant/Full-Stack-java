19d371852b27054273a577203ed9f8ada85d42e9
vedantmore27 <vedantmore894@gmail.com>
2025-11-06 12:57:16 +0530

    code of the day

---
+++ JS/Function.js
@@ -0,0 +1,11 @@
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+let n4 = 4;
+let n5 = 5;
+
+function name(a, b, c, d, e){
+    return n1 + n2 + n3 + n4 + n5;
+}
+let ans = name(n1, n2, n3, n4, n5);
+console.log("ans")
