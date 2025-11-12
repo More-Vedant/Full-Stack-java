3a4fbf84c950699a69d153d75d84482b451f56e3
vedantmore27 <vedantmore894@gmail.com>
2025-11-12 13:10:22 +0530

    code of the day

---
+++ JS/HighOrderFunction.js
@@ -0,0 +1,60 @@
+// let number = 'hellooo';
+
+// let newArr = Array.from(number, (_, i) => i + 1);
+
+// let data = newArr.map((element, index, array) => {
+//     return element < 5 ? array[index] : null
+// });
+// console.log(data);
+
+
+// let str=["mango", "apple", "kiwi", "watermelon", "mango", "kiwi", "banana", "grapes"]
+
+// let data1 = str.filter((element, index, array) => {
+//     return index = 1
+// });
+// console.log(data1);
+
+// let data3=str.forEach((el,i,arr)=>{
+//     el=="apple"?console.log(el,i):null
+// });
+// console.log(data3);
+
+
+// let str1 = [
+//     'mango',
+//     'apple',
+//     'kiwi',
+//     'watermelon',
+//     'mango',
+//     'apple',
+//     'kiwi',
+//     'banana',
+//     'grapes'
+// ];
+
+// let data4 = str1.reduce((acc,curr)=>{
+//     console.log(curr);
+//     acc[curr] = (acc[curr] || 0) + 1;
+//     return acc;
+// }, {});
+// console.log(data4);
+
+
+// let arr = [
+//     2,
+//     7,
+//     0,
+//     3,
+//     1,
+//     6,
+//     4,
+//     8
+// ];
+
+// let data = arr.reduce((acc,curr)=>{
+//     curr=curr + 2
+//     return console.log(curr); 
+// }, {});
+
+
---
+++ JS/MathsMethod.js
@@ -0,0 +1,11 @@
+// let num1 = 3;
+// let num2 = 8;
+// let ans=Math.abs(num1-num2);
+// console.log(ans);
+
+
+// let num = 8.4;
+// console.log(Math.round(num));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+
