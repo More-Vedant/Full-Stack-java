2d4b16ac7d4f65a4816bda00678a370b83043f25
vedantmore27 <vedantmore894@gmail.com>
2025-12-17 10:39:49 +0530

    code of the day

--- JAVASCRIPT/Local_Storage/Local_Storage.js
+++ JAVASCRIPT/Local_Storage/Local_Storage.js
@@ -1,49 +1,111 @@
-const API = `https://fakestoreapi.com/products`;
+const API=`https://fakestoreapi.com/products`
+
+let cartData=JSON.parse(localStorage.getItem('cartData'))||[];
 
 async function ApiCall() {
-
     try {
-        const res = await fetch(API);
-        const data = await res.json();
+        const res=await fetch(API)
+        const data=await res.json()
 
-        dataAppend(data);
+        dataAppend(data)
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
 
-ApiCall();
+window.onload = function () {
+    updateCartCount();
+};
+ApiCall()
 
-function dataAppend(value) {
-    console.log(value);
-
-    const mainDiv = document.querySelector('#mainData');
+function dataAppend(value){
+    const mainDiv =document.querySelector('#mainData');
 
     value?.forEach((el) => {
-        const childDiv = document.createElement('Div');
-        const id = document.createElement('h3');
-        const img = document.createElement('img');
-        const title = document.createElement('h3');
-        const description = document.createElement('h3');
-        const price = document.createElement('h3');
-        const category = document.createElement('h3');
-        const AddtoCart=document.createElement('button')
+        const childDiv=document.createElement('div');
+        const id=document.createElement('h3');
+        const img=document.createElement('img');
+        const title=document.createElement('h3');
+        const description=document.createElement('h3');
+        const price=document.createElement('h3');
+        const category=document.createElement('h3');
+        const addBtn=document.createElement('button')
+        const plusBtn=document.createElement('button')
+        const minusBtn=document.createElement('button')
+        const qty=document.createElement('span')
 
-        id.innerText = el.id;
+        plusBtn.style='display:none'
+        minusBtn.style='display:none'
+        qty.style='display:none'
 
-        img.src = el.image;
+        plusBtn.innerText='+'
+        minusBtn.innerText='-'
+        qty.innerText="1";
 
-        title.innerText = el.title;
+        id.innerText=el.id;
+        img.src=el.image;
+        title.innerText=el.title;
+        description.innerText=el.description
+        price.innerText=`₹ ${el.price}`;
+        category.innerText=el.category;
+        addBtn.innerText='Add'
 
-        description.innerText = el.description;
+        const existingProduct = cartData.find(item => item.id === el.id);
+        if (existingProduct) {
+            addBtn.style.display = "none";
+            plusBtn.style.display = "inline-block";
+            minusBtn.style.display = "inline-block";
+            qty.style.display = "inline-block";
+            qty.innerText = existingProduct.qty; 
+        }
 
-        price.innerText = el.price;
+        addBtn.addEventListener('click',()=>{
+            addBtn.style='display:none'
+            plusBtn.style='display:inline-block'
+            qty.style='display:inline-block'
+            minusBtn.style='display:inline-block'
 
-        category.innerText = el.category;
+            cartData.push({ ...el, qty: 1 });
+            qty.innerText = 1;
+            localStorage.setItem('cartData' ,JSON.stringify(cartData) )
+            updateCartCount();
+        })
 
-        AddtoCart.innerText='Add'
+        plusBtn.addEventListener('click',()=>{
+            const item = cartData.find(item => item.id === el.id);
+            item.qty += 1;
+            qty.innerText = item.qty;
+            minusBtn.disabled = false;
+            localStorage.setItem('cartData' ,JSON.stringify(cartData) )
+            updateCartCount();
+        })
 
-        childDiv.append(id,img,title,description,price,category);
-        mainDiv.append(childDiv);
+        minusBtn.addEventListener('click',()=>{
+            const item=cartData.find(item=>item.id===el.id);
+            item.qty-=1
+
+            if(!item||item.qty<=0){
+                addBtn.style.display = "inline-block";
+                plusBtn.style.display = "none";
+                minusBtn.style.display = "none";
+                qty.style.display = "none";
+            }else{
+                addBtn.style.display = "none";
+                plusBtn.style.display = "inline-block";
+                minusBtn.style.display = "inline-block";
+                qty.style.display = "inline-block";
+                qty.innerText = item.qty;
+                if (item.qty === 1) {
+                    minusBtn.disabled = true;
+                } else {
+                    minusBtn.disabled = false;
+                }
+            }
+            localStorage.setItem('cartData' ,JSON.stringify(cartData) )
+            updateCartCount();
+        })
+
+        childDiv.append(id,img,title,description,price,category,addBtn,plusBtn,qty,minusBtn);
+        mainDiv.append(childDiv)
     });
 }
