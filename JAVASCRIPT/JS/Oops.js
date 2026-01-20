506f479f99c00a8bd188293cfc6d8345733eacf0
vedantmore27 <vedantmore894@gmail.com>
2026-01-20 10:31:15 +0530

    code of the day

--- JAVASCRIPT/JS/Oops.js
+++ JAVASCRIPT/JS/Oops.js
@@ -11,3 +11,20 @@ class Car {
 let c1 = new Car('desire', 'suzuki');
 console.log(c1);
 console.log(c1.showDetails());
+
+class SUV extends Car {
+    constructor(x, y) {
+        super(x, y);
+        this.buyer = "Vedant";
+    }
+}
+
+class miniSUV extends SUV {
+    constructor(x, y) {
+        super(x, y);
+        this.type = "mini-suv";
+    }
+}
+
+let car2 = new miniSUV("audi q3", "audi");
+console.log(car2);
