9d21f0a7611b46d96e95fee884a59b3208f62059
vedantmore27 <vedantmore894@gmail.com>
2026-02-05 11:11:44 +0530

    code of the day

--- REACT/Second_project/src/App.jsx
+++ REACT/Second_project/src/App.jsx
@@ -1,13 +1,39 @@
+// import { useState } from 'react';
+// import './App.css';
+// import { Countes } from './Components/countes';
+
+// export function App() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <>
+//     <h1 id="count">count {count}</h1>
+//     <Countes xyz={{setCount}}/>
+//     </>
+//   );
+// }
+
 import { useState } from 'react';
 import './App.css';
-import { Countes } from './Components/countes';
+import { Countes } from './Components/Countes';
+import { Data } from './Components/Data';
+import { AxiosData } from './Components/Axios';
 
 export function App() {
   const [count, setCount] = useState(0);
+
+  const API = '';
+
+  const data = [
+
+  ];
+
   return (
     <>
-    <h1 id="count">count {count}</h1>
-    <Countes xyz={{setCount}}/>
+    <Countes xyz={{ setCount,count }} />
+
+    <Data props={{ data }} />
+
+    <AxiosData url={API} />
     </>
   );
 }
--- REACT/Second_project/src/Components/Countes.jsx
+++ REACT/Second_project/src/Components/Countes.jsx
@@ -1,5 +1,23 @@
+// export const Countes = ({ xyz }) => {
+//     console.log("xyz",xyz);
+//     const handleDec = () => {
+//         xyz.setCount((prv) => prv - 1);
+//     };
+
+//     const handleInc = () => {
+//         xyz.setCount((prv) => prv + 1);
+//     };
+
+//     return (
+//         <>
+//         <button onClick={handleDec}>dec</button>
+//         <button onClick={handleInc}>inc</button>
+//         </>
+//     );
+// };
+
 export const Countes = ({ xyz }) => {
-    console.log("xyz",xyz);
+    console.log(xyz);
     const handleDec = () => {
         xyz.setCount((prv) => prv - 1);
     };
@@ -10,6 +28,7 @@ export const Countes = ({ xyz }) => {
 
     return (
         <>
+        <h1 id="count">count {xyz.count}</h1>
         <button onClick={handleDec}>dec</button>
         <button onClick={handleInc}>inc</button>
         </>
