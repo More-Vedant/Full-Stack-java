a334ff832353a3d16c6adfd67daac8f7e6c72c26
vedantmore27 <vedantmore894@gmail.com>
2026-02-09 11:09:23 +0530

    code of the day

--- REACT/Todos/src/Components/TodoInput.jsx
+++ REACT/Todos/src/Components/TodoInput.jsx
@@ -16,17 +16,19 @@ export const TodoInput = () => {
         };
 
         setTodo((prev) => [...prev, data]);
-        console.log(todo);
+        setText('');
     };
-
-    console.log(todo);
 
     return (
         <>
-        <input type="text" onChange={(el) => setText(el.target.value)}
+        <input
+            value={text}
+            name="todo_input"
+            type="text"
+            onChange={(e) => setText(e.target.value)}
         />
             <button onClick={handleAdd}>Add</button>
-            <Todos props={{ todo }} />
+            <Todos props={{ todo, setTodo }} />
         </>
     );
 };
