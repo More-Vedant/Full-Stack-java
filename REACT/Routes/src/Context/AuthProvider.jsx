00137fc765f54bd7cb231efa03313f9f101f5d5c
vedantmore27 <vedantmore894@gmail.com>
2026-02-26 11:03:32 +0530

    code of the day

--- REACT/Routes/src/Context/AuthProvider.jsx
+++ REACT/Routes/src/Context/AuthProvider.jsx
@@ -1,1 +1,16 @@
-import { createContext, useState } from "react";
+import { Children, createContext, useState } from "react";
+
+export const AuthValContext = createContext(null);
+
+export const AuthContext = ({ Children }) => {
+    const [auth, setAuth] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    return (
+        <AuthValContext.Provider
+        value={{ auth, setAuth, loading, setLoading, error, setError }}
+        >
+            {Children}
+        </AuthValContext.Provider>
+    );
+};
