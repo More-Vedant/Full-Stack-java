12f0d98ae8bd6590279193cb204abcf631a432a6
vedantmore27 <vedantmore894@gmail.com>
2026-03-10 16:35:20 +0530

    Code of the day

--- REDUX/Redux1/src/App.jsx
+++ REDUX/Redux1/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Account } from './'
+import { Account } from './Components/Account';
 
 export const App = () => {
   return (
---
+++ REDUX/Redux1/src/Components/Account.jsx
@@ -0,0 +1,29 @@
+import { myStore } from '../';
+import { DEPOSITE, WITHDRAW } from '../';
+import { useState } from 'react';
+
+export const Account = () => {
+  const { dispatch, subscribe } = myStore;
+
+  const [jugad, setJugad] = useState(0);
+
+  const handleDeposite = () => {
+  dispatch({ type: DEPOSITE, payload: 10 });
+};
+
+subscribe(() => {
+  setJugad(jugad + 1);
+});
+
+const handleWithdraw = () => {
+  dispatch{( type: WITHDRAW, payload: 20});
+};
+
+return (
+<>
+  <h1>Account {myStore.getState().amount}</h1>
+  <button onclick={handleDeposite}>deposite</button>
+  <button onclick={handlewithdraw}>withdraw</button>
+</>
+);
+};
